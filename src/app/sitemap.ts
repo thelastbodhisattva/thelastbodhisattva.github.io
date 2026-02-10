c4b4eba15a6a16d23611c40b03c2d619f84a4f84
Maitreya <codenamexyfer@gmail.com>
2026-02-10 05:27:02 +0300

    missing export const dynamic = "force-static"

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import type { MetadataRoute } from "next";
 import { getAllPosts } from "@/lib/blog";
 
+export const dynamic = "force-static";
+
 const BASE_URL = "https://saammaaeel.online";
 
 export default function sitemap(): MetadataRoute.Sitemap {
